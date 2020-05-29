2ff6b37cde447ad8d385bae25c93a90337f5d787
Colin Sindle <csindle@gmail.com>
2020-05-29 19:49:03 +0200

    Install script and gem updates.

---
+++ INSTALL.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Document to build and run site locally (on Ubuntu).
+
+sudo apt install ruby-bundler ruby-dev 
+
+bundle install
+bundle update
+
+# Serve locally:
+bundle exec jekyll serve --livereload
